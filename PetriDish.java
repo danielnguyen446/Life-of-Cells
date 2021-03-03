218dab0a855d0c9bf47d28141a08196f79afd4ea
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-03-02 23:32:52 -0800

    Create PetriDish.java

---
+++ PetriDish.java
@@ -0,0 +1,28 @@
+/** 
+ * Name: Daniel Nguyen
+ * ID: A16129027
+ * Email: d7nguyen@ucsd.edu
+ * Sources used: none
+ * 
+ * This file contains the class to display the petri dish that contains
+ * all of the cells in the current iteration. Each space on the dish can 
+ * contain either a living cell or an empty space.
+ */
+
+import java.util.*;
+
+/**
+ * This class contains methods to initialize and create the structure of the 
+ * petri dish.
+ */
+public class PetriDish
+{
+    /**
+     * This constructor initializes the board.
+     */
+    public PetriDish(String[][] board);
+    {
+        //format is "{CELL_TYPE} {MASS}" 
+    }
+   
+}
