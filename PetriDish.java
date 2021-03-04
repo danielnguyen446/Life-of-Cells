3e9ccfebe9a939c3fc1b6fc49191da923c776916
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-03-03 21:30:03 -0800

    Update PetriDish.java

--- PetriDish.java
+++ PetriDish.java
@@ -2,12 +2,14 @@
  * Name: Daniel Nguyen
  * ID: A16129027
  * Email: d7nguyen@ucsd.edu
- * Sources used: none
+ * Sources used: tutor help(Ashley Kou, Emma Yuan)
  * 
  * This file contains the class to display the petri dish that contains
  * all of the cells in the current iteration. Each space on the dish can 
  * contain either a living cell or an empty space.
  */
+
+import java.util.*;
 
 /**
  * This class contains methods to initialize and create the structure of the 
@@ -21,10 +23,56 @@ public class PetriDish
     /**
      * This constructor initializes the board.
      */
-    public PetriDish(String[][] board);
+    public PetriDish(String[][] board)
     {
-        //format is "{CELL_TYPE} {MASS}" 
-        
+        //iterate through the 2d array of strings by row and column
+        for(int row=0; row<board.length; row++)
+        {
+            for(int column=0; column<board[row].length; column++)
+            {
+                //create a temporary string array called petri
+                String[] petri = board[row][column].trim().split(" ");
+                //petri will hold type and mass
+                String CELL_TYPE = petri[0];
+                int MASS = Integer.parseInt(petri[1]);
+                
+                //initialize the dish
+                dish = new Cell[row][column];
+                
+                //create the dish depending on the class/type of cells
+                if (CELL_TYPE.equals("null"))
+                {
+                    //edge case
+                    continue;
+                }
+                if (CELL_TYPE.equals("CellMoveUp"))
+                {
+                    dish[row][column] = new CellMoveUp(row, column, MASS);
+                }
+                else if (CELL_TYPE.equals("CellStationary"))
+                {
+                    dish[row][column] = new CellStationary(row, column, MASS);
+                }
+                else if (CELL_TYPE.equals("CellDivide"))
+                {
+                    dish[row][column] = new CellDivide(row, column, MASS);
+                }
+                else if (CELL_TYPE.equals("CellMoveToggle"))
+                {
+                    dish[row][column] = new CellMoveToggle(row, column, MASS);
+                }
+                else if (CELL_TYPE.equals("CellMoveDiagonal"))
+                {
+                    dish[row][column] = new CellMoveDiagonal(row, 
+                    column, MASS);
+                }
+                else if (CELL_TYPE.equals("CellMoveToggleChild"))
+                {
+                    dish[row][column] = new CellMoveToggleChild(row, 
+                    column, MASS);
+                }
+            }
+        }
     }
-  
+    
 }
